b8e0cc53096cc84e59a00608d83f687375e03303
DDorka02 <dobszaydorka@gmail.com>
2024-03-21 11:39:06 +0100

    felkesz

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ const ELEMEK = document.querySelector(".tartalom")
 //const KEP = document.querySelector("")
     //console.log(KEP)
 
-/*const KEPEK =document.querySelector("")
-    KEPEK.addEventListener("mouseover", function ()) {
-    KEP.classList.add("kiv") }
+const KEPEK = document.querySelectorAll(".kepek img")  
+for (let index = 0; index < KEPEK.length; index++) {
+    KEPEK[index].addEventListener("mouseover", function () {
+    KEPEK[index].classList.add("kiv") })
+}
 
-const MEGMUTAT =document.querySelector(".megmutat")
+/*const MEGMUTAT =document.querySelector(".megmutat")
     MEGMUTAT.addEventListener("click", function ()) {
-        
-    }
-    */
+
+    }*/
